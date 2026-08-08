876405a78376ee739ab64fe3bb1a6ba7bba1f7f0
it's <chukwudifunsofor@gmail.com>
2026-08-08 11:58:37 +0200

    feat: add max and min limit on registration data

--- server/DTO/RegisterDTO.cs
+++ server/DTO/RegisterDTO.cs
@@ -2,11 +2,18 @@
 // Username
 // Password
 // Prevents exposing internal models.
+using System.ComponentModel.DataAnnotations;
+
 namespace server.DTO;
 
 public class RegisterDto
 {
+    [Required]
+    [StringLength(30, MinimumLength = 8)]
     public string Username { get; set; } = null!;
+
+    [Required]
+    [StringLength(64, MinimumLength = 8)]
     public string Password { get; set; } = null!;
     // public string Role { get; set; } = null!;
     // public bool Banned {get; set; } = false;
