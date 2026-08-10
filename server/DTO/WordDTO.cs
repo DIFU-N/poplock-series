6175d8184c4b3bb83d6afc18df469e8836f43682
it's <chukwudifunsofor@gmail.com>
2026-08-10 17:56:15 +0200

    feat: add posts, comments and likes, fxns and objects.

--- server/DTO/WordDTO.cs
+++ server/DTO/WordDTO.cs
@@ -1,13 +1,31 @@
 namespace server.DTO;
 
-public class WordDto
+public class PostDto
 {
-    public string? Id { get; set; } = string.Empty;
+    public string Id { get; set; } = string.Empty;
     public string Title { get; set; } = string.Empty;
 
     // Tiptap JSON comes in here
     public object Body { get; set; } = default!;
     public DateTime Date { get; set; }
     public string Subtitle { get; set; } = string.Empty;
-    public string Group { get; set; } = string.Empty;
+    public string? HeaderImage { get; set; } = string.Empty;
+
+    public string AuthorId {get; set;} = string.Empty;
+}
+
+// response dto
+public class CommentDto
+{
+    public string? Id { get; set; } = string.Empty;
+    public string Body { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+    public string PostId {get; set;} = string.Empty;
+    public string AuthorId {get; set;} = string.Empty;
+}
+
+// requestdto
+public class CreateCommentDto
+{
+    public string Body {get; set;} = string.Empty;
 }
