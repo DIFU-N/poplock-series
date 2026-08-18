ef299eee98ebe6d2b56e2f5547272c2fa9f45c59
it's <chukwudifunsofor@gmail.com>
2026-08-18 14:14:57 +0200

    feat: add email to register form

--- utils/types/auth.ts
+++ utils/types/auth.ts
@@ -1,16 +1,18 @@
 import { User } from "./user";
 
 export type AuthResponse = {
-    token: string;
-    user: User;
-    message: string;
-}
+  token: string;
+  user: User;
+  message: string;
+};
 
 interface AuthFormValues {
-    username: string;
-    password: string;
+  username: string;
+  password: string;
 }
 
 export type LoginFormInitialValues = AuthFormValues;
 
-export type RegisterFormInitialValues = AuthFormValues;
+export interface RegisterFormInitialValues extends AuthFormValues {
+  email: string;
+}
