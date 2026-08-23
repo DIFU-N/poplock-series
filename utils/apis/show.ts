c2b5aff5a0386cd9873279a8fc389fea0c0fb0dd
it's <chukwudifunsofor@gmail.com>
2026-08-23 15:41:19 +0200

    feat: get show by id

--- utils/apis/show.ts
+++ utils/apis/show.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { API_BASE } from "./auth";
+import { Rating } from "../types/rating";
+import { setFeaturedInputValues } from "../types/shows";
 
 export const fetchGenres = async () => {
   const response = await axios.get(`${API_BASE}/genres`);
@@ -7,7 +9,11 @@ export const fetchGenres = async () => {
 };
 
 export const searchForShow = async (query: string) => {
-  const response = await axios.post(`${API_BASE}/show/search`, { query });
+  const response = await axios.get(`${API_BASE}/show/search`, {
+    params: { query },
+  });
+  console.log(response);
+
   return response.data;
 };
 
@@ -18,5 +24,20 @@ export const importShow = async (tvMazeId: number) => {
 
 export const getAllShows = async () => {
   const response = await axios.get(`${API_BASE}/show`);
+  return response.data;
+};
+
+export const setFeaturedShows = async ({ showIds }: setFeaturedInputValues) => {
+  const response = await axios.put(`${API_BASE}/show/featured/`, { showIds });
+  return response.data;
+};
+
+export const rateShow = async (input: Rating) => {
+  const response = await axios.post(`${API_BASE}/rating`, { input });
+  return response.data;
+};
+
+export const getShowById = async (id: string) => {
+  const response = await axios.post(`${API_BASE}/show/search/in`, { id });
   return response.data;
 };
