a4afd89158898e2fdb070d2c4aafffc77f66538d
it's <chukwudifunsofor@gmail.com>
2026-08-24 22:55:23 +0200

    feat: fix to accept a top score of 10

--- components/atoms/SignalBars.tsx
+++ components/atoms/SignalBars.tsx
@@ -1,9 +1,21 @@
 export default function SignalBars({ signal }: { signal: number }) {
   return (
     <span className="signal-bars flex items-end gap-0.75">
-      {[1, 2, 3, 4, 5].map((n) => (
-        <i key={n} className={n <= signal ? "on" : ""} />
-      ))}
+      {[1, 2, 3, 4, 5].map((n) => {
+        const value = signal - (n-1) * 2;
+
+        return (
+          <i key={n} 
+          className={
+            value >= 2 
+            ? "full" 
+            : value === 1 
+              ? "half"
+              : ""
+            } />
+        )
+      }
+    )}
     </span>
   );
 }
