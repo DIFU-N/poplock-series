629679ac7174db70753240993fd1eace7d526af4
it's <chukwudifunsofor@gmail.com>
2026-08-24 22:31:12 +0200

    fix: change route to accept auth header

--- utils/apis/rating.ts
+++ utils/apis/rating.ts
@@ -1,33 +1,32 @@
-import axios from "axios";
-import { RateShowRequest, Rating, UpdateRateShowRequest } from "../types/rating";
-import { API_BASE } from "./auth";
+import { RateShowRequest, UpdateRateShowRequest } from "../types/rating";
+import { api } from "./auth";
 
 export const getAverageRating = async (showId: string) => {
-  const response = await axios.get(`${API_BASE}/rating/average/${showId}`);
+  const response = await api.get(`/rating/average/${showId}`);
   return response.data;
 };
 
 export const rateShow = async (input: RateShowRequest) => {
-  const response = await axios.post(`${API_BASE}/rating`, { input });
+  const response = await api.post(`/rating`, input);
   return response.data;
 };
 
 export const getDadamansRating = async (showId: string) => {
-  const response = await axios.get(`${API_BASE}/rating/${showId}/dadaman`);
+  const response = await api.get(`/rating/${showId}/dadaman`);
   return response.data;
 };
 
 export const getAllUsersRatings = async () => {
-  const response = await axios.get(`${API_BASE}/rating/me`);
+  const response = await api.get(`/rating/me`);
   return response.data;
 };
 
 export const updateRating = async (input: UpdateRateShowRequest) => {
-  const response = await axios.put(`${API_BASE}/rating`, { input });
+  const response = await api.put(`/rating`, input);
   return response.data;
 };
 
 export const getUserRating = async (showId: string) => {
-  const response = await axios.get(`${API_BASE}/rating/${showId}`);
+  const response = await api.get(`/rating/${showId}/me`);
   return response.data;
 };
