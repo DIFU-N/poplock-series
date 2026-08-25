4d1ba7df7c04b941e4527c68dcdb13313804c7c6
it's <chukwudifunsofor@gmail.com>
2026-08-25 12:57:45 +0200

    type: add inclusive type of rating and show

--- utils/types/rating.ts
+++ utils/types/rating.ts
@@ -1,3 +1,5 @@
+import { Show } from "./shows";
+
 export type Rating = {
   id: string;
   userId: string;
@@ -20,3 +22,12 @@ export type UpdateRateShowRequest = {
   id: string;
   score: number;
 };
+
+export type RatingWithShow = {
+  id: string;
+  score: number;
+  show: Show;
+  updatedAt: string;
+};
+
+export type GetRatingWithShowResponse = RatingWithShow[];
