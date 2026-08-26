78990fc8113132f7c442285a1f0e9c06859084ac
it's <chukwudifunsofor@gmail.com>
2026-08-26 13:03:33 +0200

    feat: add tvmazeepisode class

--- server/Models/Episode.cs
+++ server/Models/Episode.cs
@@ -28,3 +28,31 @@ public class Episode
 
     public string? Summary { get; set; }
 }
+
+public class TvMazeEpisode
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public int Id { get; set; }
+
+    // public int TvMazeId { get; set; }
+
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string ShowId { get; set; } = string.Empty;
+
+    public string Title { get; set; } = string.Empty;
+
+    public int Season { get; set; }
+
+    public int Number { get; set; }
+
+    public DateTime? AirDate { get; set; }
+
+    public string? AirTime { get; set; } = null;
+
+    public DateTime? AirStamp { get; set; }
+
+    public int? Runtime { get; set; }
+
+    public string? Summary { get; set; }
+}
