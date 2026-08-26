65b24eaf03872cae789fd6887de1ea6be8432148
it's <chukwudifunsofor@gmail.com>
2026-08-26 12:59:01 +0200

    feat: spread episode and add null check

--- server/Repositories/EpisodeRepositories.cs
+++ server/Repositories/EpisodeRepositories.cs
@@ -1,5 +1,7 @@
+using Microsoft.VisualBasic;
 using MongoDB.Driver;
 using server.Data;
+using server.DTO;
 using server.Models;
 
 namespace server.Repositories;
@@ -7,10 +9,18 @@ namespace server.Repositories;
 public class EpisodeRepository
 {
     private readonly IMongoCollection<Episode> _episodes;
+    private readonly ShowRepository _shows;
+    private readonly TvMazeService _tvMaze;
 
-    public EpisodeRepository(MongoDbContext context)
+    public EpisodeRepository(
+        MongoDbContext context,
+        ShowRepository shows,
+        TvMazeService tvMazeService
+    )
     {
         _episodes = context.Episode;
+        _shows = shows;
+        _tvMaze = tvMazeService;
     }
 
     public async Task<List<Episode>> GetAll()
@@ -35,5 +45,42 @@ public class EpisodeRepository
         var filter = Builders<Episode>.Filter.Eq(r => r.TvMazeId, episode.TvMazeId);
 
         await _episodes.ReplaceOneAsync(filter, episode, new ReplaceOptions { IsUpsert = true });
+    }
+
+    public async Task<List<ScheduleShowDTO>> GetSchedules()
+    {
+        var shows = await _shows.GetScheduledFeaturedShows();
+
+        var tasks = shows.Select(async show =>
+        {
+            var nextEpisode = await _tvMaze.GetNextEpisode(show.TvMazeId);
+
+            if (nextEpisode == null)
+            {
+                return null;
+            }
+
+            return new ScheduleShowDTO
+            {
+                Show = show,
+                NextEpisode = new Episode
+                {
+                    AirDate = nextEpisode.AirDate,
+                    AirStamp = nextEpisode.AirStamp,
+                    AirTime = nextEpisode.AirTime,
+                    Number = nextEpisode.Number,
+                    ShowId = nextEpisode.ShowId,
+                    Runtime = nextEpisode.Runtime,
+                    Season = nextEpisode.Season,
+                    Summary = nextEpisode.Summary,
+                    Title = nextEpisode.Title,
+                    TvMazeId = nextEpisode.Id,
+                },
+            };
+        });
+
+        var results = await Task.WhenAll(tasks);
+
+        return results.OfType<ScheduleShowDTO>().OrderBy(x => x!.NextEpisode!.AirStamp).ToList();
     }
 }
