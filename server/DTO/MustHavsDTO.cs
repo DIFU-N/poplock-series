96e8fdcf40e4dac1a736bd09ce0910fcc9cbe81e
it's <chukwudifunsofor@gmail.com>
2026-08-27 18:19:16 +0200

    feat: add must hav dtos

--- server/DTO/MustHavsDTO.cs
+++ server/DTO/MustHavsDTO.cs
@@ -1,3 +1,5 @@
+using server.Models;
+
 namespace server.DTO;
 
 public class MustHavDTO
@@ -8,5 +10,27 @@ public class MustHavDTO
 
     public string Description { get; set; } = string.Empty;
 
-    public List<string> ShowIds { get; set; } = [];
+    public List<int> TvMazeIds { get; set; } = [];
 }
+
+public class GetMustHavsResponse
+{
+    public string? Id { get; set; } = string.Empty;
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public List<SomeOfShow> Shows { get; set; } = [];
+}
+
+// public class MustHavsShow
+// {
+//     public string? Id { get; set; } = string.Empty;
+
+//     public string Name { get; set; } = string.Empty;
+
+//     public string Description { get; set; } = string.Empty;
+
+//     public List<string> ShowIds { get; set; } = [];
+// }
