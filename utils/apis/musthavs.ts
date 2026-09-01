a455af12e1ebc59ad7bd4d1a2f5cf18f968f57b5
it's <chukwudifunsofor@gmail.com>
2026-09-01 11:00:43 +0200

    feat: get musthavs

--- utils/apis/musthavs.ts
+++ utils/apis/musthavs.ts
@@ -1,20 +1,25 @@
 import axios from "axios";
-import { API_BASE } from "./auth";
+import { api, API_BASE } from "./auth";
 import { MustHav } from "../types/musthavs";
 import { ShowRanking } from "../types/showRanking";
 
 export const addMustHav = async (musthav: MustHav) => {
-  const response = await axios.post(`${API_BASE}/musthavs/add`, { musthav });
+  const response = await api.post(`/musthavs/add`, musthav);
   return response.data;
 };
 
 export const addTopTenFamFriends = async (
   showRanking: ShowRanking,
-  token: string,
+  // token: string,
 ) => {
-  const response = await axios.post(`${API_BASE}/musthavs/add/top10`, {
+  const response = await api.post(`/musthavs/add/top10`, {
     showRanking,
-    token,
+    // token,
   });
+  return response.data;
+};
+
+export const getMustHavs = async () => {
+  const response = await api.get(`/musthavs/all`);
   return response.data;
 };
