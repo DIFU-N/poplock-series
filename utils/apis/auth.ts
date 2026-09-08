5e9f3cdb16e85ecb9ecb76f14523ad96a1c8e305
it's <chukwudifunsofor@gmail.com>
2026-09-08 23:26:33 +0200

    feat: use backend url

--- utils/apis/auth.ts
+++ utils/apis/auth.ts
@@ -6,7 +6,7 @@ import {
 } from "../types/auth";
 import { useAuthStore } from "../store/zustand-hooks/useAuthStore";
 
-export const API_BASE = "http://localhost:5191/api";
+export const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5191/api";
 
 export const api = axios.create({
   baseURL: API_BASE,
