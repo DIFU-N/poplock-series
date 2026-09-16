a520b9118a7271a389316b9dfa59f8b27da11224
it's <chukwudifunsofor@gmail.com>
2026-09-16 11:19:08 +0200

    feat: add ff rank dto

---
+++ server/DTO/FFRanksDTO.cs
@@ -0,0 +1,12 @@
+namespace server.DTO;
+
+public class FFRankDTO
+{
+    public string ShowId { get; set; } = string.Empty;
+
+    public string ShowName { get; set; } = string.Empty;
+
+    public string ShowImage { get; set; } = string.Empty;
+
+    public int Points { get; set; }
+}
