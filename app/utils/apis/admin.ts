e403873f74d2a14b4d18c95e86b786d45adf3e05
it's <chukwudifunsofor@gmail.com>
2026-09-17 14:39:01 +0200

    refactor: change api cals to use api instance

--- app/utils/apis/admin.ts
+++ app/utils/apis/admin.ts
@@ -1,27 +1,28 @@
-import axios from "axios";
-import { API_BASE } from "./auth";
+import { api } from "./auth";
 
 export const makeAdmin = async (username: string) => {
-  const response = await axios.post(`${API_BASE}/make-admin`, { username });
-  return response.data;
-};
-
-export const deleteAccount = async (username: string) => {
-  const response = await axios.post(`${API_BASE}/delete-account`, { username });
-  return response.data;
-};
-
-export const banAccount = async (username: string) => {
-  const response = await axios.post(`${API_BASE}/ban-account`, { username });
-  return response.data;
-};
-
-export const unbanAccount = async (username: string) => {
-  const response = await axios.post(`${API_BASE}/unban-account`, { username });
+  const response = await api.post(`admin/make-admin`, { username });
   return response.data;
 };
 
 export const createInvite = async (name: string) => {
-  const response = await axios.post(`${API_BASE}/create-invite`, { name });
+  const response = await api.post(`admin/create-invite`, {
+    name,
+  });
+  return response.data;
+};
+
+export const deleteAccount = async (username: string) => {
+  const response = await api.post(`admin/delete-account`, { username });
+  return response.data;
+};
+
+export const banAccount = async (username: string) => {
+  const response = await api.post(`admin/ban-account`, { username });
+  return response.data;
+};
+
+export const unbanAccount = async (username: string) => {
+  const response = await api.post(`admin/unban-account`, { username });
   return response.data;
 };
