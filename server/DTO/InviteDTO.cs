fad10d99239f21da327b5bed27864ab73183387e
it's <chukwudifunsofor@gmail.com>
2026-09-19 18:57:58 +0200

    feat: add dto for create invite request

--- server/DTO/InviteDTO.cs
+++ server/DTO/InviteDTO.cs
@@ -5,3 +5,8 @@ public class CreateInviteRequest
     public string Token { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
 }
+
+public class AdminCreateInviteRequest
+{
+        public string Name { get; set; } = string.Empty;
+}
