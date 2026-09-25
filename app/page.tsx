d332789621cb40b92fb550bf6b7fd3b6595276d9
it's <chukwudifunsofor@gmail.com>
2026-09-25 21:47:54 +0200

    chore: remove comments

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,11 @@
 "use client";
-import Footer from "@/components/atoms/Footer";
-import Header from "@/components/atoms/Header";
-// import ForYou from "@/components/molecules/ForYou";
-import BestWeekly from "@/components/molecules/home/BestWeekly";
-import HotShows from "@/components/molecules/home/HotShows";
-import MustHavs from "@/components/molecules/home/MustHav";
-// import useStore from "@/utils/store/store";
-// import { useBearStore } from "@/utils/store/zustand-hooks/useBearStore";
-import { useShowStore } from "@/utils/store/zustand-hooks/useShowStore";
-import { useEffect } from "react";
+import Footer from "@/app/components/atoms/Footer";
+import Header from "@/app/components/atoms/Header";
+import BestWeekly from "@/app/components/molecules/home/BestWeekly";
+import HotShows from "@/app/components/molecules/home/HotShows";
+import MustHavs from "@/app/components/molecules/home/MustHav";
 
 export default function Home() {
-  // const bears = useStore(useBearStore, (state) => state.bears);
-  // const addOne = useStore(useBearStore, (state) => state.addABear);
-  // const getValues = useShowStore();
-
-  // useEffect(() => {
-  //   getValues.execute();
-  //   console.log("okay let me check this", getValues.data);
-  // }, [getValues, getValues.data]);
   return (
     <div>
       <div>
