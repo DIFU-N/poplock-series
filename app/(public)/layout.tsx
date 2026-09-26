7f94316ed8fb7d1799db5ce1967429007b3df1a3
it's <chukwudifunsofor@gmail.com>
2026-09-26 15:42:37 +0200

    refactor: remove the bg color

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -12,7 +12,7 @@ export default function PublicLayout({
     <div className="flex min-h-screen flex-col">
       <Header />
 
-      <main className="flex-1 bg-black">{children}</main>
+      <main className="flex-1">{children}</main>
 
       <Footer />
     </div>
